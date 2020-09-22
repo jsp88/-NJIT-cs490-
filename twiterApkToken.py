7aeb8ce8b093dc1a2624c4e06dc539d6d1b16ddc
EC2 Default User <ec2-user@ip-172-31-9-109.ec2.internal>
2020-09-22 01:09:16 +0000

    update .py

--- twiterApkToken.py
+++ twiterApkToken.py
@@ -1,5 +1,5 @@
 import os
-#import flask
+import flask
 from tweepy import OAuthHandler
 from tweepy import API
 from tweepy import Cursor
@@ -8,7 +8,7 @@ from collections import Counter
 from os.path import join, dirname
 from dotenv import load_dotenv
 import sys
-#app = flask.Flask(__name__)
+app = flask.Flask(__name__)
 
 dotenv_path = join(dirname(__file__), 'tweet.env')
 load_dotenv(dotenv_path)
@@ -20,33 +20,42 @@ access_token_secret=os.environ["TOKEN_TOKEN"]
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
-
-
 auth_api = API(auth)
-account_list = "@Spotify "
-user = auth_api.get_user(account_list)
-screen_name = user.screen_name
-user_name = user.name
-description = user.description
-user_tweets = auth_api.user_timeline(account_list)
-end_date = datetime.now() - timedelta(days=30)
-tweet_list = []
-tweet_time = []
-for twitt in user_tweets:
-  if twitt.created_at < end_date:
-    break
-  tweet_list.append(twitt.text)
-  tweet_time.append(twitt.created_at)
+
+@app.route('/')
+
+
+def index():
+  AccountName = "@Spotify "
+  user = auth_api.get_user(AccountName)
+  screen_name = user.screen_name
+  user_name = user.name
+  description = user.description
+  user_tweets = auth_api.user_timeline(AccountName)
+  end_date = datetime.now() - timedelta(days=30)
+  tweet_list = []
+  tweet_time = []
   
-  list_len = len(tweet_list)
-  time_len = len(tweet_time)
-  for i in range(0,list_len):
-    print(twitt.text)
-    print(twitt.created_at)
-    print(screen_name)
-    print(user_name)
-    print(description)
-    print(tweet_list[i])
-    print(tweet_time[i])
+  for twitt in user_tweets:
+    if twitt.created_at < end_date:
+      break
+    tweet_list.append(twitt.text)
+    tweet_time.append(twitt.created_at)
+    
+  return flask.render_template(
+    "index.html",
+    screen_name = screen_name,
+    user_name = user_name,
+    description = description,
+    tweet_list = tweet_list,
+    tweet_time = tweet_time,
+    list_len = len(tweet_list),
+    time_len = len(tweet_time)
+    )
+    
+app.run(
+    port = int(os.getenv("PORT",8080)),
+    host = os.getenv('IP','0.0.0.0')
+    )
+    
     
